4e1992952c1efec126e6aabfea98b77be4ae0395
iceCreamCoder <jared.web@live.com>
2019-11-19 20:48:50 -0800

    change

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const PORT = process.env.PORT;
 const bcrypt = require('bcrypt-nodejs');
 const app = express();
 const cors = require("cors");
@@ -9,7 +8,6 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-// const imageurl = require('./controllers/imageurl')
 
 
 const db = knex({
@@ -56,6 +54,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App listening on port ${process.env.PORT}`)
 })
